b045a3bf90b0cc6618d827c184c897baf52c9a02
motoha0827 <82797330+motoha0827@users.noreply.github.com>
2021-11-18 19:14:47 +0900

    a

---
+++ src/Main.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Token from './token';
+
+class Main extends React.Component {
+    render() {
+
+        let tokenList1 = {
+            //この中にimage:〇〇みたいな感じで画像のID入れていきたい。でもその処理をどこに書けばいいのか分からない案件
+        }
+        let tokenList2 = {
+
+        }
+        let tokenList3 = {
+
+        }
+        return (
+            <div className='main-wrapper'>
+                <div className='main'>
+                    <div className='copy-container'>
+                        <h1>NFT一覧のところだよ！</h1>
+                    </div>
+                    <div className='token-container'>
+                        <div className="parents">
+                            <div className="div-parent1">
+                                <p>parent1</p>
+                                {tokenList1.map((tokenItem) => {
+                                    return (
+                                        <Token
+                                        name={tokenItem.name}
+                                        image={tokenItem.image}
+                                        />
+                                    );
+                                    })}
+                            </div>
+                        </div>
+                        <div className="parents">
+                            <div className="div-parent2">
+                                <p>parent2</p>
+                                {tokenList2.map((tokenItem) => {
+                                    return (
+                                        <Token
+                                        name={tokenItem.name}
+                                        image={tokenItem.image}
+                                        />
+                                    );
+                                    })}
+                            </div>
+                        </div>
+                        <div className="parents">
+                            <div className="div-parent3">
+                                <p>parent3</p>
+                                {tokenList3.map((tokenItem) => {
+                                    return (
+                                        <Token
+                                        name={tokenItem.name}
+                                        image={tokenItem.image}
+                                        />
+                                    );
+                                    })}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main;
