4fe01610b565bfd1bbd9ca1a33ab23815c20002e
shima <shmn7iii@gmail.com>
2021-11-21 12:52:55 +0900

    Update Issue.js

--- src/Issue.js
+++ src/Issue.js
@@ -2,35 +2,49 @@ import React from 'react';
 import firebase from './firebase';
 import axios from 'axios';
 
+
+
 class Issue extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             user: null,
+            uid: null,
             base64: null,
             imageURL: null
         };
 
         this.onFileChange = this.onFileChange.bind(this);
-      }
+    }
 
 
     componentDidMount() {
         firebase.auth().onAuthStateChanged(user => {
-            this.setState({
-                user: user
-            });
+            if(user){
+                this.setState({
+                    user: user,
+                    uid: user.uid
+                });
+            }
         });
     }
 
-    issueToken() {
-        axios.post("https://tap-api.shmn7iii.net/v2/tokens",
-            {
-                uid: this.state.user.uid,
-                token_data: this.state.base64
-            }
-        )
+
+    issueToken(st) {
+        if(st.user != null){
+            var issuetoken = axios.post("https://tap-api.shmn7iii.net/v2/tokens",{
+                uid: st.uid,
+                token_data: st.base64}
+            ).then(response => {
+                console.log(response);
+                return response.data;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        }
     }
+
 
     onFileChange(e) {
         const imageFile = e.target.files[0];
@@ -58,13 +72,15 @@ class Issue extends React.Component {
     render() {
         return (
             <div>
-                <input type="file" accept="image/png" onChange={
-                    (e) => {
-                        this.onFileChange(e)
-                    }
-                } />
-                <img id="imageid" src={this.state.imageURL} alt="gazo-" />
-                <button onClick={this.issueToken}>発行</button>
+                <div>
+                    <img id="imageid" src={this.state.imageURL} alt="gazo-" />
+                </div>
+                <div>
+                    <input type="file" accept="image/png" onChange={ (e) => { this.onFileChange(e) } } />
+                </div>
+                <div>
+                    <button onClick={this.issueToken(this.state)}>発行</button>
+                </div>
             </div>
         )
     }
