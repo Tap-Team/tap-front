aadc45c6b36f6ddb09203892d8379448d44eaa04
motoha0827 <82797330+motoha0827@users.noreply.github.com>
2021-11-24 16:43:52 +0900

    2

--- src/Top.js
+++ src/Top.js
@@ -4,6 +4,7 @@ import imgBc from './img/blockchain-3446557_1920.jpg';
 import imgStop from './img/james-lee-y15-SmlLTe8-unsplash.jpg';
 import imgWant from './img/michael-browning-LmxoicGTLDQ-unsplash.jpg';
 import imgIdea from './img/mark-fletcher-brown-nN5L5GXKFz8-unsplash.jpg';
+import imgNFT from './img/chain-5842371_1920.jpg';
 
 class Top extends React.Component {
     render() {
@@ -18,8 +19,8 @@ class Top extends React.Component {
                     <p>SERVICE</p>
                     <div className="service-card1">
                         <div className="gray-box"></div>
-                        <img className="service-img" src={imgBc} alt=""/>
-                        <div className="service-explain">
+                        <img className="service-img1" src={imgNFT} alt=""/>
+                        <div className="service-explain1">
                             <h1>NFT技術を用いた</h1>
                             <h1>『作品管理プラットフォーム』</h1>
                             <p></p>
@@ -28,16 +29,16 @@ class Top extends React.Component {
                     </div>
                     <div className="service-card2">
                         <div className="gray-box"></div>
-                        <img className="service-img" src={imgBc} alt=""/>
-                        <div className="service-explain">
+                        <img className="service-img2" src={imgBc} alt=""/>
+                        <div className="service-explain2">
                             <h1>NFTと暗号通貨の切り離し</h1>
                             <p>暗号通貨の投資的性質に対するハードルを云々</p>
                         </div>
                     </div>
                     <div className="service-card3">
                         <div className="gray-box"></div>
-                        <img className="service-img" src={imgIdea} alt=""/>
-                        <div className="service-explain">
+                        <img className="service-img1" src={imgIdea} alt=""/>
+                        <div className="service-explain1">
                             <h1>『俺が先だがシステム』</h1>
                             <p>メモ：唯一性より良い言葉あるかも。タイムスタンプとか専門用語出てきたらリンクっぽくしてモーダル表示で解説あると便利かも</p>
                             <p>トークンのタイムスタンプを根拠に作品の唯一性を保証</p>
@@ -45,16 +46,16 @@ class Top extends React.Component {
                     </div>
                     <div className="service-card4">
                         <div className="gray-box"></div>
-                        <img className="service-img" src={imgWant} alt=""/>
-                        <div className="service-explain">
+                        <img className="service-img2" src={imgWant} alt=""/>
+                        <div className="service-explain2">
                             <h1>『それ欲しいシステム』</h1>
                             <p>トークンの移転機能</p>
                         </div>
                     </div>
                     <div className="service-card5">
                         <div className="gray-box"></div>
-                        <img className="service-img" src={imgStop} alt=""/>
-                        <div className="service-explain">
+                        <img className="service-img1" src={imgStop} alt=""/>
+                        <div className="service-explain1">
                             <h1>『ちょっと待てシステム』</h1>
                             <p>不正トークン発行時に「ちょっと待て」</p>
                         </div>
